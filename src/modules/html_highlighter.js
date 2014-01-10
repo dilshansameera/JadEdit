351ff2471b96a3233641918dac3695f9367d7a5c
Won Song <wys.io@live.com>
2014-01-10 15:55:12 -0500

    html highlighter now detects class attribute and string

--- src/modules/html_highlighter.js
+++ src/modules/html_highlighter.js
@@ -2,8 +2,9 @@
  * ===================================================================== */
 
 var HTML_HIGHLIGHTER = (function (UTIL) {
-	var htmlTagRegex = /<.*?>/g;
-	var classRegex = /(?![<](.*?))(class)(?=(.*?)[>])/;
+	var htmlTagRegex = /<.*?>/gi;
+	var classRegex = /(?![<](.*?))(class) ?[=](?=(.*?)[>])/i;
+	var stringRegex = /(?![<](.*?))['|"](.*?)['|"](?=(.*?)[>])/i;
 
 	var htmlHighlighter = {};
 
@@ -14,25 +15,53 @@ var HTML_HIGHLIGHTER = (function (UTIL) {
 
 		if (tagsFound != null) {
 			for (var index = 0; index < tagsFound.length; index++) {
-				var nonTag =  currentLine.substring(0, currentLine.indexOf(tagsFound[index]));
+				// Checks if there is anything comes before finding a html tag, and highlight it as a plain text.
+				var nonTag = currentLine.substring(0, currentLine.indexOf(tagsFound[index]));
 				if (nonTag.length > 0) {
 					nonTag = createCodeElement('plain', nonTag);
 				}
 				result += nonTag;
 
-				result += createCodeElement('keyword', tagsFound[index])
+				var currentTagFound = tagsFound[index];
 
+				// Checks if currently being processed html tag has any thing needs to be highlighted
+				if (classRegex.test(currentTagFound)) {
+					var classLocation = classRegex.exec(currentTagFound).index;
+
+					result += createCodeElement('keyword',
+						currentTagFound.substring(0, classLocation));
+					result += createCodeElement('attribute', 'class');
+					currentTagFound = currentTagFound.substring(classLocation
+						+ 'class'.length, currentTagFound.length)
+				}
+
+				if (stringRegex.test(currentTagFound)) {
+					var string = stringRegex.exec(currentTagFound);
+					var stringLocation = string.index;
+
+					result += createCodeElement('keyword',
+						currentTagFound.substring(0, stringLocation));
+					result += createCodeElement('string', string[0]);
+					currentTagFound = currentTagFound.substring(stringLocation
+						+ string[0].length, currentTagFound.length)
+				}
+
+				result += createCodeElement('keyword', currentTagFound);
+
+
+				// Set the current line as remaining texts that comes after the first keyword found
 				currentLine = currentLine.substring(currentLine.indexOf(tagsFound[index]) +
 					tagsFound[index].length, currentLine.length);
 			}
 
+			// Handles lines that does not have a closing tag in the same line
 			if (currentLine.length != 0) {
 				result += createCodeElement('plain', currentLine);
 			}
+			// Handles lines that does not have any html tags
 		} else {
 			result += createCodeElement('plain', currentLine);
 		}
-
 
 		return {
 			'processedLine': result + '\n',
